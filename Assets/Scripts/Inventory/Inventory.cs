af8847cecacfde54b33afda8baef6680b1be312e
LogZep <logzep@gmal.com>
2025-09-20 12:22:28 +0500

    (fix): Добавлена проверка на соответствующую длину isFull и slots

--- Assets/Scripts/Inventory/Inventory.cs
+++ Assets/Scripts/Inventory/Inventory.cs
@@ -6,4 +6,12 @@ public class Inventory : MonoBehaviour
 {
     public bool[] isFull;
     public GameObject[] slots;
+
+    private void Awake()
+    {
+        if(isFull.Length != slots.Length) // Защитит при логической ошибке
+        {
+            Debug.LogError("Slots lenght is not equal isFull lenght!");
+        }
+    }
 }
